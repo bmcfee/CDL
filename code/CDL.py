ece7d1cb3d725cabc7116d66cafda7e6496821bf
Brian McFee <brm2132@columbia.edu>
2013-03-01 08:30:20 -0500

    first helper functions

--- code/CDL.py
+++ code/CDL.py
@@ -4,5 +4,36 @@ CREATED:2013-03-01 08:19:26 by Brian McFee <brm2132@columbia.edu>
 
 Convolutional Dictionary Learning
 
-
 '''
+
+import numpy
+import scipy.sparse
+
+def separateComplex(X):
+    '''
+    Separate the real and imaginary components of a matrix
+
+    See also: combineComplex()
+
+    Input:
+        complex d-by-n matrix X
+
+    Output:
+        real 2d-by-n matrix Y = [ real(X) ; imag(X) ]
+    '''
+    return numpy.vstack((X.real, X.imag))
+
+def combineComplex(Y):
+    '''
+    Combine the real and imaginary components of a matrix
+
+    See also: separateComplex()
+
+    Input:
+        real 2d-by-n matrix Y = [ real(X) ; imag(X) ]
+
+    Output:
+        complex d-by-n matrix X
+    '''
+    d = Y.shape[0] / 2
+    return Y[:d] + 1.j * Y[d:]
